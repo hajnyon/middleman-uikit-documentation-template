1503989a58d6697b8a6d66dfd2dc95203f46f77a
Ondrej Hajny <insensaty@gmail.com>
2020-07-15 23:04:32 +0200

    feature: heading anchors

--- template/source/javascripts/site.js
+++ template/source/javascripts/site.js
@@ -1,6 +1,7 @@
 // This is where it all goes :)
 
-document.addEventListener("DOMContentLoaded", () => {
-  var a = document.getElementById('edit-page-link');
-  a.setAttribute('href', a.href + "?return_url=" + encodeURIComponent(window.location.href));
+document.addEventListener('DOMContentLoaded', () => {
+    anchors.add();
+    var a = document.getElementById('edit-page-link');
+    a.setAttribute('href', a.href + '?return_url=' + encodeURIComponent(window.location.href));
 });
